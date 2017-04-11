a16137c91300bcb6dc42ca830dde6520bad69e3a
jacegem <jacegem@gmail.com>
2017-04-11 15:21:46 +0900

    added torrentwiz site

--- torrent/models.py
+++ torrent/models.py
@@ -8,7 +8,7 @@ class Magnet(models.Model):
     url = models.CharField(max_length=255, db_index=True)
     category = models.CharField(max_length=100, db_index=True)
     title = models.TextField()
-    magnet = models.TextField()
+    magnet = models.CharField(max_length=100, db_index=True)
     reg_date = models.DateTimeField(db_index=True, auto_now_add=True)
 
     def get_absolute_url(self):
--- torrent/views.py
+++ torrent/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 import re
 import urllib.request
-import urllib
 import logging
+import socket
 from urllib.parse import quote
 from bs4 import BeautifulSoup
 from django.utils.http import urlencode
@@ -18,8 +18,9 @@ from django.utils.html import escape
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
 
+
 def rss(request):
-    latest_magnet_list = Magnet.objects.order_by('-reg_date')[:100]
+    latest_magnet_list = Magnet.objects.order_by('-reg_date')[:1000]
 
     trackers = '&tr=udp://tracker.openbittorrent.com:80&tr=http://megapeer.org:6969/announce&tr=http://mgtracker.org:2710/announce&tr=http://tracker.files.fm:6969/announce&tr=http://tracker.flashtorrents.org:6969/announce&tr=http://tracker.mg64.net:6881/announce&tr=http://tracker.nwps.ws:6969/announce&tr=http://tracker.ohys.net/announce&tr=http://tracker.tfile.me/announce&tr=udp://9.rarbg.com:2710/announce&tr=udp://9.rarbg.me:2710/announce&tr=udp://coppersurfer.tk:6969/announce&tr=udp://tracker.coppersurfer.tk:6969&tr=udp://tracker.leechers-paradise.org:6969&tr=udp://exodus.desync.com:6969/announce&tr=udp://open.coppersurfer.com:1337/announce'
 
@@ -58,8 +59,8 @@ def collect_tfreeca():
     url_home = 'http://www.tfreeca2.com/'
     url_ref_map = {
         'movie': 'board.php?mode=list&b_id=tmovie',
-        'drama': 'board.php?mode=list&b_id=tdrama',
-        'variety': 'board.php?mode=list&b_id=tent',
+        'tv': 'board.php?mode=list&b_id=tdrama',
+        'tv': 'board.php?mode=list&b_id=tent',
         'tv': 'board.php?mode=list&b_id=tv',
         'ani': 'board.php?mode=list&b_id=tani',
         'music': 'board.php?mode=list&b_id=tmusic',
@@ -86,9 +87,11 @@ def collect_tfreeca():
             bs_torrent = get_bs(url_home, torrent_src)
             magnet = bs_torrent.find('div', {'class': 'torrent_magnet'}).find('a')['href']
             # return render(request, 'torrent/collect.html', {'result': bs.prettify()})
-            magnet, created = Magnet.objects.get_or_create(title=title, magnet=magnet, url=url_home + href,
-                                                           category=category)
-            result.append(magnet)
+            # magnet, created = Magnet.objects.get_or_create(title=title, magnet=magnet, url=url_home + href, category = category)
+
+            obj = save_data(title, magnet, href, category)
+
+            result.append(obj)
 
             # if created:
             #     logging.debug(title, " added")
@@ -99,63 +102,67 @@ def collect_tfreeca():
 
 
 def collect(request, site='all'):
+    result = collect_torrentwiz()
     result = collect_tfreeca()
-    # result = collect_torrenters()
+
     return render(request, 'torrent/collect.html', {'result': result})
 
 
-def get_bs(url_home, url_ref):
+def get_bs(url_home, url_ref=""):
     url = url_home + url_ref
     req = urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
     try:
         html = urllib.request.urlopen(req, timeout=10).read()
         bs = BeautifulSoup(html, 'html.parser', from_encoding='utf-8')
-    except:
-        logger.error('Something went wrong!' + url)
-
+    except socket.timeout:
+        print(url)
     return bs
 
 
-def collect_torrenters():
-    url_home = 'http://torrentersg.com/bbs/'
+def collect_torrentwiz():
+    url_home = 'https://torrentwiz4.com/'
     url_ref_map = {
-        'tv': 'board.php?bo_table=tr_pbtv'
-        , 'tv': 'board.php?bo_table=tr_ftv'
-        , 'movie': 'board.php?bo_table=tr_kmovie'
-        , 'movie': 'board.php?bo_table=tr_fmovie'
+        'movie': 'torrent_movieko',
+        'movie': 'torrent_movielatest',
+        'tv': 'torrent_drama',
+        'tv': 'torrent_ent',
+        'tv': 'torrent_sisa',
+        'music': 'torrent_kpop',
+        'music': 'torrent_pop',
+        'ani': 'torrent_ani',
+        'comic': 'torrent_cartoon',
     }
 
     result = list()
 
     for category, url_ref in url_ref_map.items():
         bs = get_bs(url_home, url_ref)
-        bs_trs = bs.find('table', {'id': 'tbl_board'}).findAll('tr')
+        bs_trs = bs.find('table', {'class': 'list-pc'}).find('tbody').findAll('tr')
 
         for bs_tr in bs_trs:
-            if 'class' in bs_tr.attrs:
-                tr_class = bs_tr.attrs['class'][0]
-                if tr_class != 'bg0' and tr_class != 'bg1':
-                    continue
-            href = bs_tr.find('a')['href']
-            
+            td_num = bs_tr.find('td', {'class': 'td_num'}).text
+            if td_num == 'AD':
+                continue
+            href = bs_tr.find('td', {'class': 'list-subject'}).find('a')['href']
             # 상세 페이지 이동
-            bs_content = get_bs(url_home, href)
-            file_links = bs_content.findAll('li', {'class': 'file_link'})
-            alink = file_links[1].find('a')
-            title = alink['title']
-            href_js = alink['href']
-            m = re.search('javascript:dnload\(\'(.*?)\'', href_js)
-            href = m.group(1)
-            magnet, created = Magnet.objects.get_or_create(title=title, url=url_home + href, category=category)
-            if created:
-                print(title, " added")
-                result.append(magnet)
-            else:
-                print(title, " exist")
+            bs_content = get_bs(href)
+            title = bs_content.find('div', {'class': 'view-wrap'}).find('h1').text
+            magnet = bs_content.find('a', {'class': 'view_file_download'})['href']
 
-            url = url_home + href
-            req = urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
-            html = urllib.request.urlopen(req).read()
-            bs = BeautifulSoup(html, 'lxml', from_encoding='utf-8')
+            obj = save_data(title, magnet, href, category)
+
+            result.append(obj)
+
+            print(title)
 
     return result
+
+
+def save_data(title, magnet, url, category):
+    try:
+        obj = Magnet.objects.get(magnet=magnet)
+    except Magnet.DoesNotExist:
+        obj = Magnet(title=title, magnet=magnet, url=url, category=category)
+        obj.save()
+
+    return obj
